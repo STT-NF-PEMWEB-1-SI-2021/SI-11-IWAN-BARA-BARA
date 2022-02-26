cc953778864f4e76cce9d899615fc420aae818a9
IwanBara <91892202+IwanBara@users.noreply.github.com>
2022-02-26 15:21:25 +0700

    Add files via upload

---
+++ SMS2pertemuan1/variable_user.php
@@ -0,0 +1,14 @@
+<?php
+// Definisikan variable
+$nama = 'Iwan Bara Bara';
+$umur = 19;
+$berat = 60;
+
+echo 'Nama : ' .$nama;
+echo '<br/>Umur : ' .$umur. 'Tahun';
+echo '<br/>Berat : ' .$berat. ' Kg ';
+
+echo "<br/> Hello $nama Apa kabar";
+echo "<br/> Hai nama saya $nama Umur saya $umur Berat badan saya $berat Kg, Salam kenal semua nya";
+echo "<hr>";
+?>
